7e770bd0d6c00b5f44e3e5a0253d4ac792e6a63a
Moibe <moi_estrello@hotmail.com>
2024-02-26 21:55:34 -0600

    Access list

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 import gradio as gr
 
+#Funciones adicionales
+
+def authenticate(username, password):
+    usuarios = [("usuario1", "contraseña1"), ("usuario2", "contraseña2")]
+    for u, p in usuarios:
+        if username == u and password == p:
+            return True
+    return False
+
+#Función principal
 def greet(name):
     return "Hello, hola, aloha " + name + "!!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch(auth=("admin", "pass1234"))
+
+#iface.launch(auth=("admin", "pass1234"))
+
+iface.launch(auth=authenticate)
