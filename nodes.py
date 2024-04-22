149b11a52bfbdcfac3763499555449e58352ae42
Moibe <moi.estrello@gmail.com>
2024-04-22 17:18:22 -0600

    Supercharge

--- nodes.py
+++ nodes.py
@@ -1,3 +1,4 @@
-hf_token = "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"
+#Buscar hf_token como secret de hf o verc como no lo ocupe.
+#hf_token = "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"
 validator = "Moibe/sulku"
 work = "picswap"
